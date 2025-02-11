0b907322012f9aced71c88a19bc65d647bf85017
Jhonata Godoy <88396979+jhonatagd@users.noreply.github.com>
2025-02-10 22:01:07 -0300

    update files

---
+++ listaUm/exercicioCinco/main.go
@@ -0,0 +1,9 @@
+//Faça um Programa que converta metros para centímetros.
+package main
+
+func(){
+	var metros
+
+	fmt.Print("Digite metros para converter em centímetros: ")
+	fmt.Scan(&notaUmBi)
+}
