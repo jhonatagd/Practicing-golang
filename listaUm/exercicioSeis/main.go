da61d14e7b2854b3d5d60d273f133c6ee1933fe9
Jhonata Godoy <88396979+jhonatagd@users.noreply.github.com>
2025-02-13 19:16:42 -0300

    update file seis

---
+++ listaUm/exercicioSeis/main.go
@@ -0,0 +1,14 @@
+//Faça um Programa que peça o raio de um círculo
+//calcule e mostre sua área.
+package main
+
+func main(){
+	var raio float
+	var formulaRaio
+
+	fmt.Print("digite um raio de um circulo: ")
+	fmt.Scan(&raio)
+	formulaRaio = raio / 2
+
+	fmt.Print("Valor do diametro: ", formulaRaio)
+}
